d92ded79f6084adc0d65283904b4b9edaf593806
DohosMate <dohosmail@gmail.com>
2022-05-02 11:56:08 +0200

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'tachyons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App= ()=>{
+  return(
+    <div className="tc bg-light-blue br3 pa3 ma2 grow">
+      <h2>App 1 trining starting at 2021.05.02</h2>
     </div>
-  );
+  )
 }
-
 export default App;
